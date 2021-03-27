889291a570b86f5bc42527501d108f626ba7e682
Francisco Sousa <francisco.sousa@hey.com>
2021-03-27 02:20:11 +0000

    wip

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -8,11 +8,18 @@
     <Loadable loader="{() => import('./Initiative/index.svelte')}" />
   </Route>
 
+  <Route path="/encounter">
+    <Loadable loader="{() => import('./Encounter/index.svelte')}" />
+  </Route>
+
   <Route path="/">
     <main class="flex flex-col items-center">
       <h1 class="mb-40 text-3xl">DM tools</h1>
 
-      <Link class="u-button" to="/initiative">Initiative Tracker</Link>
+      <div class="flex flex-col items-center space-y-8">
+        <Link class="u-button" to="/initiative">Initiative Tracker</Link>
+        <Link class="u-button" to="/encounter">Encounter Calculator</Link>
+      </div>
     </main>
   </Route>
 </Router>
